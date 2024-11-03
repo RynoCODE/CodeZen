cc1b4d0a9238496bcb8b86ba92201fa54468a993
RynoCODE <rohit.roychoudhury13@gmail.com>
2024-11-03 17:46:22 +0530

    Add user_id field to codes_run and code_submit models

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center text-white h-96 w-full">
       <h1 className="text-4xl font-serif font-bold">Leetcode</h1>
+      <Appbar />
       <h1>Home</h1>
       <a href="/auth/signup">signup</a>
       <a href="/auth/signin">signin</a>
