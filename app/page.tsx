fdce96887fba25c05963cf3027b9f7d38dbc5f96
RynoCODE <rohit.roychoudhury13@gmail.com>
2024-11-08 12:58:48 +0530

    Remove Appbar component from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center text-white h-96 w-full">
       <h1 className="text-4xl font-serif font-bold">Leetcode</h1>
-      <Appbar />
       <h1>Home</h1>
       <a href="/auth/signup">signup</a>
       <a href="/auth/signin">signin</a>
