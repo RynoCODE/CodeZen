7c5a649dd4a79c5ecee444f642a0f81807cc9938
RynoCODE <rohit.roychoudhury13@gmail.com>
2024-12-23 05:53:11 +0000

    Load environment variables using dotenv in database connection files

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 
 import { MongoClient } from 'mongodb';
-
+require('dotenv').config()
 const uri = process.env.MONGODB_URI || '';
 const options = {};
 
--- lib/redisClient.ts
+++ lib/redisClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'redis';
-
+require('dotenv').config()
 const client = createClient({
   username: 'default',
   password: process.env.NEXT_PUBLIC_REDIS_PASSWORD,
